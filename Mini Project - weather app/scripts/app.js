88340b3905ae7fdeb4ed3f114abc3eabf01ea6ef
Chi Mei Wang <kimmy0929@gmail.com>
2020-02-29 13:28:23 +0900

    updating UI based on the data

--- Mini Project - weather app/scripts/app.js
+++ Mini Project - weather app/scripts/app.js
@@ -1,5 +1,29 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
 
+const updateUI = data => {
+  // const cityInfo = data.cityInfo;
+  // const weather = data.weather;
+
+  //! destructuring: easy way to get properties from an object and store them in a constance in a same name
+  const { cityInfo, weather } = data;
+
+  // update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityInfo.EnglishName}</h5>
+    <div class="my-3">${weather[0].WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather[0].Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  // remove d-none if present
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
 const updateCity = async city => {
   const cityInfo = await getCity(city);
   const weather = await getWeather(cityInfo.Key);
@@ -23,8 +47,6 @@ cityForm.addEventListener('submit', e => {
 
   // update the ui with new city
   updateCity(city)
-    .then(data => {
-      console.log(data);
-    })
+    .then(data => updateUI(data))
     .catch(error => console.log(error));
 });
