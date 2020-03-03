02b4a75d0f6d20ed1cbe85479d06e5da2d87ce3f
Chi Mei Wang <kimmy0929@gmail.com>
2020-03-04 00:26:00 +0900

    refactor weather app using oojs

---
+++ Mini Project - weather app/scripts/app_refactor.js
@@ -0,0 +1,72 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const forecast = new Forecast();
+
+const updateUI = data => {
+  // const cityInfo = data.cityInfo;
+  // const weather = data.weather;
+
+  //! destructuring: easy way to get properties from an object and store them in a constance in a same name
+  const { cityInfo, weather } = data;
+
+  // update night/day image
+  // let timeSrc = null;
+  // if (weather.IsDayTime) {
+  //   timeSrc = 'img/day.svg';
+  // } else {
+  //   timeSrc = 'img/night.svg';
+  // }
+  // time.setAttribute('src', timeSrc);
+
+  //! ternary operator (same as above)
+  let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+  time.setAttribute('src', timeSrc);
+
+  // update weather icon
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+
+  // update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityInfo.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  // remove d-none if present
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
+
+cityForm.addEventListener('submit', e => {
+  // prevent default action
+  e.preventDefault();
+
+  // get city value and empty input field
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update the ui with new city
+  forecast
+    .updateCity(city)
+    .then(data => updateUI(data))
+    .catch(error => console.log(error));
+
+  // set local storage
+  localStorage.setItem('city', city);
+});
+
+if (localStorage.getItem('city')) {
+  forecast
+    .updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(error => console.log(error));
+}
