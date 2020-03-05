897adba3911d67f2a8a0d13000f05439c9235679
Chi Mei Wang <kimmy0929@gmail.com>
2020-03-05 23:12:21 +0900

    format date with date-fns

--- Mini Project - chatroom/scripts/app.js
+++ Mini Project - chatroom/scripts/app.js
@@ -3,7 +3,7 @@ const chatList = document.querySelector('.chat-list');
 
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('general', 'Kimmy');
+const chatroom = new Chatroom('gaming', 'Kimmy');
 
 // get the chats and render
 chatroom.getChat(data => chatUI.render(data));
--- Mini Project - chatroom/scripts/ui.js
+++ Mini Project - chatroom/scripts/ui.js
@@ -5,11 +5,14 @@ class ChatUI {
     this.list = list;
   }
   render(data) {
+    const time = dateFns.distanceInWordsToNow(data.created_at.toDate(), {
+      addSuffix: true,
+    });
     const html = `
     <li class="list-group-item">
       <span class="username"> ${data.username} </span>
-      <span class="message"> ${data.message} </span>
-      <div class="time">${data.created_at.toDate()}</div>
+      <span class="time text-right">${time}</span>
+      <div class="message ml-2"> ${data.message} </div>
     </li>
     `;
     this.list.innerHTML += html;
